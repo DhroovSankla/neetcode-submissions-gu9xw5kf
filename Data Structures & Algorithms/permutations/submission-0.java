db7d4b710b48ccd36f9f65f793608e9cf6b88965
DhroovSankla <dhroovsankla2004@gmail.com>
2026-06-24 15:08:18 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> rs = new ArrayList<>();
+        boolean[] vd = new boolean[nums.length];
+        bt(nums,vd,new ArrayList<>(),rs);
+        return rs;
+    }
+
+    private void bt(int[] nums, boolean[] vd, List<Integer> curr, List<List<Integer>> rs) {
+        if(curr.size() == nums.length) {
+            rs.add(new ArrayList<>(curr));
+            return;
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            if(vd[i]) continue;
+
+            curr.add(nums[i]);
+            vd[i] = true;
+
+            bt(nums,vd,curr,rs);
+
+            curr.remove(curr.size()-1);
+            vd[i] = false;
+        }
+    }
+}
