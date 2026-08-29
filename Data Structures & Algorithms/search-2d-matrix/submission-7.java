ca58d1327f78966f513eccf0a2d483cb0c76c03e
Dhroov <dhroovsankla2004@gmail.com>
2026-08-29 20:31:48 +0530

    Add: search-2d-matrix - submission-7

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-7.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix == null || matrix.length == 0) return false;
+
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        int l = 0;
+        int r = (row * col) -1;
+ 
+        while (l <= r) {
+            int mid = l + (r-l)/2;
+            int midVal = matrix[mid / col][mid % col];
+
+            if(midVal == target) {
+                return true;
+            }
+            else if(midVal < target) {
+                l = mid +1;
+            }
+            else {
+                r = mid -1;
+            }
+        }
+        return false;
+    }
+}
