157d5d8bc48b740843ef4c479d11aa3bd3c3232e
Dhroov <dhroovsankla2004@gmail.com>
2026-09-15 11:45:12 +0530

    Add: buy-and-sell-crypto - submission-9

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-9.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+
+        for(int i = 0; i < prices.length; i++) {
+            for(int j = i+1; j < prices.length; j++) {
+                int currProfit = prices[j] - prices[i];
+
+                if(currProfit > maxProfit) {
+                    maxProfit = currProfit;
+                }
+            }
+        }
+        return maxProfit;
+    }
+}
